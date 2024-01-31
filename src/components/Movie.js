dbd984ee07a4f3591cb36c889d5d19c046424c90
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-01-31 08:42:09 -0500

    Added css styling

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,14 @@
 const Movie = ({movie}) => {
-    console.log(movie)
 
     return (
         <div className="movie">
+            <div className="movie-image">
+                <img src={movie.image} alt={movie.name}/>
+            </div>
             <div className="movie-details">
                 <div className="name">{movie.name}</div>
-                <div className="year">{movie.year}</div>
-                <div className="rating">{movie.rating}</div>
+                <div className="year">Release year: {movie.year}</div>
+                <div className="rating">Rating: {movie.rating}</div>
             </div>
         </div>
     )
